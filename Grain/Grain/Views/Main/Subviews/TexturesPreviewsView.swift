9775e80663d769dc21f4af0e90d129503eca22b4
Vlad Antonov <blackbird@live.ru>
2025-01-15 21:40:47 +0300

    Create TexturesPreviewsView.swift

---
+++ Grain/Grain/Views/Main/Subviews/TexturesPreviewsView.swift
@@ -0,0 +1,72 @@
+import SwiftUI
+
+struct TexturesPreviewsView: View {
+    @State private var scrollToIndex: UUID?
+    @State private var visibleTexturesCategory: UUID?
+    @State private var visibleItems: Set<UUID> = []
+
+    var didTapOnPreview: ((Texture) -> Void)?
+
+
+    var body: some View {
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(texturesCategories) { category in
+                        Button {
+                            scrollToIndex = category.id
+                        } label: {
+                            var isUnderlined: Bool {
+                                if let visibleTexturesCategory {
+                                    return visibleTexturesCategory == category.id
+                                }
+                                return false
+                            }
+                            Text(category.title)
+                                .font(.h5)
+                                .underline(isUnderlined)
+                                .foregroundStyle(Color.textWhite.opacity(0.8))
+                        }
+
+                    }
+                }
+
+            }
+            ScrollViewReader { proxy in
+                ScrollView(.horizontal) {
+                    HStack(spacing: 4) {
+                            ForEach(texturesCategories) { category in
+                                LazyHStack(spacing: 4) {
+                                    ForEach(category.textures) { texture in
+                                        LazyHStack {
+                                            TexturePreviewView(texture: texture) {
+                                                didTapOnPreview?(texture)
+                                            }
+                                        }
+                                        .onAppear {
+                                            print("onAppear", texture.title)
+                                            visibleTexturesCategory = category.id
+                                        }
+                                    }
+                                }
+                                .id(category.id)
+                            }
+                    }
+                }
+                .scrollIndicators(.hidden)
+                .onChange(of: scrollToIndex) { _, newValue in
+                    if let newValue {
+                        withAnimation(.easeInOut(duration: 0.6)) {
+                            proxy.scrollTo(newValue, anchor: .leading)
+                        }
+                        scrollToIndex = nil
+                    }
+                }
+            }
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal, 16)
+        .background(Color.backgroundBlackSecondary.opacity(0.3))
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+}
