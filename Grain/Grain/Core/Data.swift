6a8c513d5f917785a9e6c825495c57ab9ba159fb
Vlad Antonov <blackbird@live.ru>
2025-01-15 22:57:48 +0300

    Update Data.swift

--- Grain/Grain/Core/Data.swift
+++ Grain/Grain/Core/Data.swift
@@ -1,5 +1,17 @@
 
 let texturesCategories: [TextureCategory] = [
+    .init(title: "VHS Noise", textures: [
+        .init(title: "VHS Noise 1", filename: "Textures/VHS-Noise/1", prefferedBlendMode: .exclusion),
+        .init(title: "VHS Noise 2", filename: "Textures/VHS-Noise/2", prefferedBlendMode: .exclusion),
+        .init(title: "VHS Noise 3", filename: "Textures/VHS-Noise/3", prefferedBlendMode: .exclusion),
+        .init(title: "VHS Noise 4", filename: "Textures/VHS-Noise/4", prefferedBlendMode: .exclusion),
+        .init(title: "VHS Noise 5", filename: "Textures/VHS-Noise/5", prefferedBlendMode: .exclusion),
+        .init(title: "VHS Noise 6", filename: "Textures/VHS-Noise/6", prefferedBlendMode: .exclusion),
+        .init(title: "VHS Noise 7", filename: "Textures/VHS-Noise/7", prefferedBlendMode: .exclusion),
+        .init(title: "VHS Noise 8", filename: "Textures/VHS-Noise/8", prefferedBlendMode: .exclusion),
+        .init(title: "VHS Noise 9", filename: "Textures/VHS-Noise/9", prefferedBlendMode: .exclusion),
+        .init(title: "VHS Noise 10", filename: "Textures/VHS-Noise/10", prefferedBlendMode: .exclusion)
+    ]),
     .init(title: "Grain", textures: [
         .init(title: "Grain 1", filename: "Textures/Grain/grain1", prefferedBlendMode: .exclusion),
         .init(title: "Grain 4", filename: "Textures/Grain/grain4", prefferedBlendMode: .exclusion),
