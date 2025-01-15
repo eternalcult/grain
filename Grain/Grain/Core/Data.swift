8ae7390f15310a77682055a4509027a6f2d759c5
Vlad Antonov <blackbird@live.ru>
2025-01-15 21:40:03 +0300

    Create Data.swift

---
+++ Grain/Grain/Core/Data.swift
@@ -0,0 +1,22 @@
+
+let texturesCategories: [TextureCategory] = [
+    .init(title: "Grain", textures: [
+        .init(title: "Grain 1", filename: "Textures/Grain/grain1", prefferedBlendMode: .exclusion),
+        .init(title: "Grain 4", filename: "Textures/Grain/grain4", prefferedBlendMode: .exclusion),
+        .init(title: "Grain 5", filename: "Textures/Grain/grain5", prefferedBlendMode: .exclusion),
+        .init(title: "Grain 6", filename: "Textures/Grain/grain6", prefferedBlendMode: .exclusion),
+        .init(title: "Grain 7", filename: "Textures/Grain/grain7", prefferedBlendMode: .multiply)
+    ]),
+    .init(title: "Rust", textures: [
+        .init(title: "Rust 1", filename: "Textures/Rust/rust1", prefferedBlendMode: .lighten),
+        .init(title: "Rust 2", filename: "Textures/Rust/rust2", prefferedBlendMode: .lighten),
+        .init(title: "Rust 3", filename: "Textures/Rust/rust3", prefferedBlendMode: .lighten),
+        .init(title: "Rust 4", filename: "Textures/Rust/rust4", prefferedBlendMode: .lighten),
+        .init(title: "Rust 5", filename: "Textures/Rust/rust5", prefferedBlendMode: .lighten),
+        .init(title: "Rust 6", filename: "Textures/Rust/rust6", prefferedBlendMode: .lighten),
+        .init(title: "Rust 7", filename: "Textures/Rust/rust7", prefferedBlendMode: .lighten),
+        .init(title: "Rust 8", filename: "Textures/Rust/rust8", prefferedBlendMode: .lighten),
+        .init(title: "Rust 9", filename: "Textures/Rust/rust9", prefferedBlendMode: .lighten),
+        .init(title: "Rust 10", filename: "Textures/Rust/rust10", prefferedBlendMode: .lighten)
+    ])
+]
