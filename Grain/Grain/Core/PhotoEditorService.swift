a9d7b3100d1d177239b51f2c94d3be8b9d739cee
Vlad Antonov <blackbird@live.ru>
2025-01-15 21:37:11 +0300

    PhotoEditorService - minor updates

--- Grain/Grain/Core/PhotoEditorService.swift
+++ Grain/Grain/Core/PhotoEditorService.swift
@@ -9,12 +9,13 @@ final class PhotoEditorService {
     /// Source Image doesn't change
     private var sourceImage: CIImage? = nil
     /// Filtered image using only for applying filters
-    var filteredImage: CIImage? = nil
+    private var filteredImage: CIImage? = nil
     /// Textured Image using only for texture applying
-    var texturedImage: CIImage? = nil
+    private var texturedImage: CIImage? = nil
     /// Final image after all updates
     var finalImage: Image? = nil
     private var texture: Texture? = nil
+    private var textureBlendMode: BlendMode? = nil
 
     func updateSourceImage(_ image: CIImage) {
         self.sourceImage = image
@@ -37,49 +38,50 @@ final class PhotoEditorService {
 
     func applyTexture(_ texture: Texture) {
         self.texture = texture
+        textureBlendMode = texture.prefferedBlendMode
         overlayTexture(texture)
     }
 
     var brightness: Filter = Brightness() {
         didSet {
-            updateFilters()
+            updateImage()
         }
     }
     var contrast: Filter = Contrast() {
         didSet {
-            updateFilters()
+            updateImage()
         }
     }
     var saturation: Filter = Saturation() {
         didSet {
-            updateFilters()
+            updateImage()
         }
     }
     var exposure: Filter = Exposure() {
         didSet {
-            updateFilters()
+            updateImage()
         }
     }
 
     var vibrance: Filter = Vibrance() {
         didSet {
-            updateFilters()
+            updateImage()
         }
     }
 
     var highlights: Filter = Highlights() {
         didSet {
-            updateFilters()
+            updateImage()
         }
     }
 
     var shadows: Filter = Shadows() {
         didSet {
-            updateFilters()
+            updateImage()
         }
     }
 
-    private func updateFilters() {
+    private func updateImage() {
         filteredImage = sourceImage
         updateBCS()
         updateExposure()
@@ -109,6 +111,63 @@ final class PhotoEditorService {
         }
     }
 
+
+    private func overlayTexture(_ texture: Texture) {
+        if let uiImage = UIImage(named: texture.filename) {
+            guard let cgImage = uiImage.cgImage, let filteredImage else { return }
+            if let resizedInputImage = resizeImageToAspectFill(image: CIImage(cgImage: cgImage), targetSize: filteredImage.extent.size) {
+                if let filter = textureBlendMode?.ciFilter {
+                    filter.backgroundImage = filteredImage
+                    filter.inputImage = resizedInputImage
+                    texturedImage = filter.outputImage
+                } else {
+                    // TODO: Normal - без филтров
+                }
+                updateFinalImage()
+            } else {
+                print("Resize texture issue") // TODO: Handle error
+            }
+        } else {
+            print("Texture doesn't exist or has wrong name") // TODO: Handle error
+        }
+    }
+
+    func changeTextureBlendMode(to blendMode: BlendMode) {
+        if textureBlendMode != blendMode {
+            textureBlendMode = blendMode
+        }
+        updateImage()
+    }
+
+
+    private func resizeImageToAspectFill(image: CIImage, targetSize: CGSize) -> CIImage? {
+        // Calculate the aspect ratio of the original image
+        let aspectRatio = image.extent.size.width / image.extent.size.height
+        var newSize = targetSize
+
+        // Scale the image to ensure it fills the target size (aspect fill)
+        if targetSize.width / targetSize.height > aspectRatio {
+            newSize.height = targetSize.width / aspectRatio
+        } else {
+            newSize.width = targetSize.height * aspectRatio
+        }
+
+        // Create a transform to scale the image to the new size
+        let transform = CGAffineTransform(scaleX: newSize.width / image.extent.size.width,
+                                          y: newSize.height / image.extent.size.height)
+
+        // Apply the transform to the image to resize it
+        let resizedImage = image.transformed(by: transform)
+
+        // Crop the image to fit exactly within the target size
+        let croppedImage = resizedImage.cropped(to: CGRect(origin: .zero, size: targetSize))
+
+        return croppedImage
+    }
+}
+
+// Private update functions
+private extension PhotoEditorService {
     // Brightness, Contrast & Saturation
     private func updateBCS() {
         let filter = CIFilter.colorControls()
@@ -148,62 +207,5 @@ final class PhotoEditorService {
         filter.intensity = 0
         filter.radius = 0
         filteredImage = filter.outputImage
-    }
-
-
-    private func overlayTexture(_ texture: Texture) {
-        if let uiImage = UIImage(named: texture.filename) {
-            guard let cgImage = uiImage.cgImage, let filteredImage else { return }
-            if let resizedInputImage = resizeImageToAspectFill(image: CIImage(cgImage: cgImage), targetSize: filteredImage.extent.size) {
-                switch texture.prefferedBlendMode {
-                case .exclusion:
-                    let filter = CIFilter.exclusionBlendMode()
-                    filter.backgroundImage = filteredImage
-                    filter.inputImage = resizedInputImage
-                    texturedImage = filter.outputImage
-                case .multiply:
-                    let filter = CIFilter.multiplyBlendMode()
-                    filter.backgroundImage = filteredImage
-                    filter.inputImage = resizedInputImage
-                    texturedImage = filter.outputImage
-                case .lighten:
-                    let filter = CIFilter.lightenBlendMode()
-                    filter.backgroundImage = filteredImage
-                    filter.inputImage = resizedInputImage
-                    texturedImage = filter.outputImage
-                }
-                updateFinalImage()
-            } else {
-                print("Resize texture issue") // TODO: Handle error
-            }
-        } else {
-            print("Texture doesn't exist or has wrong name") // TODO: Handle error
-        }
-    }
-
-
-    func resizeImageToAspectFill(image: CIImage, targetSize: CGSize) -> CIImage? {
-        // Calculate the aspect ratio of the original image
-        let aspectRatio = image.extent.size.width / image.extent.size.height
-        var newSize = targetSize
-
-        // Scale the image to ensure it fills the target size (aspect fill)
-        if targetSize.width / targetSize.height > aspectRatio {
-            newSize.height = targetSize.width / aspectRatio
-        } else {
-            newSize.width = targetSize.height * aspectRatio
-        }
-
-        // Create a transform to scale the image to the new size
-        let transform = CGAffineTransform(scaleX: newSize.width / image.extent.size.width,
-                                          y: newSize.height / image.extent.size.height)
-
-        // Apply the transform to the image to resize it
-        let resizedImage = image.transformed(by: transform)
-
-        // Crop the image to fit exactly within the target size
-        let croppedImage = resizedImage.cropped(to: CGRect(origin: .zero, size: targetSize))
-
-        return croppedImage
     }
 }
