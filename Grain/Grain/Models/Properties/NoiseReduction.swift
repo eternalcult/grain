638c2229eaacf044b20152d1f82e5ea79c279bc8
Vlad Antonov <blackbird@live.ru>
2025-01-22 15:49:21 +0300

    Update NoiseReduction.swift

--- Grain/Grain/Models/Properties/NoiseReduction.swift
+++ Grain/Grain/Models/Properties/NoiseReduction.swift
@@ -1,6 +1,6 @@
 struct NoiseReduction: ImageProperty {
     var title: String = "Noise Reduction"
-    var range: ClosedRange<Float> = 0.2...1 // Default: 0.0...1
+    var range: ClosedRange<Float> = 0.2 ... 1 // Default: 0.0...1
     var defaultValue: Float = 0.2
     var current: Float = 0.2
     let formatStyle: ImagePropertyValueFormattedStyle = .zeroTo100
