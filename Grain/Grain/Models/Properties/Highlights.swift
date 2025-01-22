744e1e703e666a56824e2864ba2141a5871846b4
Vlad Antonov <blackbird@live.ru>
2025-01-22 15:49:16 +0300

    Update Highlights.swift

--- Grain/Grain/Models/Properties/Highlights.swift
+++ Grain/Grain/Models/Properties/Highlights.swift
@@ -1,6 +1,6 @@
 struct Highlights: ImageProperty { // TODO: Check values, I'm not sure about current and range
     let title: String = "Highlights"
-    let range: ClosedRange<Float> = -1...1
+    let range: ClosedRange<Float> = -1 ... 1
     let defaultValue: Float = 1
     var current: Float = 1
     let formatStyle: ImagePropertyValueFormattedStyle = .zeroTo100
