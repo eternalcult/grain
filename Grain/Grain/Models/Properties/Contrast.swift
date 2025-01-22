da77ea48aa3cbf288a8b3956425c4695d5a6f90b
Vlad Antonov <blackbird@live.ru>
2025-01-22 15:49:04 +0300

    Update Contrast.swift

--- Grain/Grain/Models/Properties/Contrast.swift
+++ Grain/Grain/Models/Properties/Contrast.swift
@@ -1,6 +1,6 @@
 struct Contrast: ImageProperty {
     let title: String = "Contrast"
-    let range: ClosedRange<Float> = 0...2 // Default: 0...4
+    let range: ClosedRange<Float> = 0 ... 2 // Default: 0...4
     let defaultValue: Float = 1
     var current: Float = 1
     let formatStyle: ImagePropertyValueFormattedStyle = .minus100to100
