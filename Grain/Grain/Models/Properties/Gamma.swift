1c76b1dbe529c9c50089d31908e0b24e06ff751f
Vlad Antonov <blackbird@live.ru>
2025-01-22 15:49:08 +0300

    Update Gamma.swift

--- Grain/Grain/Models/Properties/Gamma.swift
+++ Grain/Grain/Models/Properties/Gamma.swift
@@ -1,6 +1,6 @@
 struct Gamma: ImageProperty {
     var title: String = "Gamma"
-    var range: ClosedRange<Float> = 0...2.0 // Default
+    var range: ClosedRange<Float> = 0 ... 2.0 // Default
     var defaultValue: Float = 1.0
     var current: Float = 1.0
     let formatStyle: ImagePropertyValueFormattedStyle = .minus100to100
