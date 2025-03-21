21e1b3e8e834fdcb20497c7ada8c4d975f13e281
Vlad Antonov <blackbird@live.ru>
2025-03-21 14:23:43 +0300

    Start creating Unit tests.

---
+++ Grain/GrainTests/Mocks.swift
@@ -0,0 +1,25 @@
+import Foundation
+@testable import Grain
+
+let mockValidFilter = Filter(
+    id: 1,
+    title: "Valid filter",
+    desc: "Valid filter description",
+    filename: "Polaroid 600"
+)
+let mockInvalidFilter = Filter(
+    id: 0,
+    title: "Invalid filter",
+    desc: "Invalid filter description",
+    filename: "mock_filter"
+)
+let mockValidTexture = Texture(
+    title: "Valid texture",
+    filename: "Textures/GrainDust/01"
+)
+let mockInvalidTexture = Texture(
+    title: " Invalid texture",
+    filename: "mock_texture"
+)
+
+
