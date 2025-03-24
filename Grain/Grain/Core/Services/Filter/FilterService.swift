8aabec680ed4d530e2fb7fb9c5f438dd75f8d8c1
Vlad Antonov <blackbird@live.ru>
2025-03-24 17:26:54 +0300

    Update logic with filter files

--- Grain/Grain/Core/Services/Filter/FilterService.swift
+++ Grain/Grain/Core/Services/Filter/FilterService.swift
@@ -5,11 +5,8 @@ import Factory
 final class FilterService: FilterServiceProtocol {
     // MARK: Properties
 
+    private let context = CIContext()
     private(set) var currentFilter: Filter?
-
-    // MARK: DI
-
-    @ObservationIgnored @Injected(\.lutsManager) private var lutsManager
 
     // MARK: Computed Properties
 
@@ -19,8 +16,17 @@ final class FilterService: FilterServiceProtocol {
 
     // MARK: Functions
 
+    func createPreview(_ filter: Filter, for image: CIImage) throws -> CGImage {
+        let cubeFilter = try createCIColorCube(for: filter)
+        cubeFilter.inputImage = image
+        if let output = cubeFilter.outputImage, let cgImage = context.createCGImage(output, from: output.extent) {
+            return cgImage
+        }
+        throw FilterServiceError.filterApplyingFailed
+    }
+
     func prepare(to newFilter: Filter, completion: () -> Void) {
-        if currentFilter?.id != newFilter.id, lutsManager.verify(newFilter) {
+        if currentFilter?.id != newFilter.id {
             currentFilter = newFilter
             completion()
         }
@@ -34,6 +40,30 @@ final class FilterService: FilterServiceProtocol {
         guard let currentFilter else {
             return processedCiImage
         }
-        return try lutsManager.apply(currentFilter, for: processedCiImage)
+        let cubeFilter = try createCIColorCube(for: currentFilter)
+        cubeFilter.inputImage = processedCiImage
+        if let output = cubeFilter.outputImage {
+            return output
+        }
+        throw FilterServiceError.filterApplyingFailed
     }
 }
+
+private extension FilterService {
+    /// Создает CoreImage фильтр для применения выбранного фильтра к изображению.
+    /// - Parameter filter: Модель фильтра, для которjго нужно создать CIColoCube фильтр
+    /// - Returns: возвращает настроенный CIColorCubeWithColorSpace
+    func createCIColorCube(for filter: Filter) throws -> CIColorCubeWithColorSpace {
+        if let fileURL = Bundle.main.url(forResource: filter.filename, withExtension: "cubedata") {
+            let filterData = try Data(contentsOf: fileURL)
+            let ciFilter = CIFilter.colorCubeWithColorSpace()
+            ciFilter.colorSpace = CGColorSpace(name: CGColorSpace.sRGB)
+            ciFilter.cubeDimension = filter.dimension
+            ciFilter.cubeData = filterData
+            return ciFilter
+        } else {
+            throw FilterServiceError.invalidFilterFileURL
+        }
+    }
+}
+
